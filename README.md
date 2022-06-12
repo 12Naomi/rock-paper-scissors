aad48d81e0795400a3c4ec63b645319c2826475b
12Naomi <105202576+12Naomi@users.noreply.github.com>
2022-06-12 16:26:46 +0100

    Added README file

--- README.md
+++
@@ -1,4 +0,0 @@
-# Rock Paper Scissors
-
-* how to play rock, paper, scissors game.
-https://www.youtube.com/watch?v=2dsHuU10udY
--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -4,6 +4,7 @@ player = input("Enter a choice (rock, paper, scissors): ")
 possible_actions = ["rock", "paper", "scissors"] 
 computer = random.choice(possible_actions) 
 print(f"\nYou chose {player}, computer chose {computer}.\n") 
+
 if player == computer: 
       print(f"Both players selected {player}. It's a tie!") 
 elif player == "rock": 
@@ -11,12 +12,14 @@ elif player == "rock":
         print("Rock smashes scissors! You win!") 
     else: 
         print("Paper covers rock! You lose.") 
+
 elif player == "paper": 
     if computer == "rock": 
         
         print("Paper covers rock! You win!") 
     else: 
         print("Scissors cuts paper! You lose.") 
+
 elif player == "scissors":
      if computer == "paper": 
          print("Scissors cuts paper! You win!")
